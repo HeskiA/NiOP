205e39543bb9ac265e451b30e2f552eb842ab549
HeskiA <toncek333@gmail.com>
2019-01-16 17:51:50 +0100

    Add files via upload

---
+++ HeskiAntonioStreamWriter/HeskiAntonioStreamWriter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HeskiAntonioStreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //reader ne writer
+            StreamReader sr = new StreamReader(@"C:\Users\Ucenik\Desktop\datoteka.txt");
+            
+            while(!sr.EndOfStream)
+            {
+
+                Console.WriteLine(sr.ReadLine());
+            }
+
+            sr.Close();
+
+            Console.ReadKey();
+        }
+    }
+}

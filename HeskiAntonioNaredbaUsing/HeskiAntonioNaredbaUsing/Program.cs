20349de0463b34e7084695b2f8ec3e76228268eb
HeskiA <toncek333@gmail.com>
2019-01-16 17:53:14 +0100

    Add files via upload

---
+++ HeskiAntonioNaredbaUsing/HeskiAntonioNaredbaUsing/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HeskiAntonioNaredbaUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite ime: ");
+            string ime = Console.ReadLine();
+            Console.Write("Unesite prezime: ");
+            string prezime = Console.ReadLine();
+            Console.WriteLine("\n-- Zapisujemo u datoteku...");
+            using (StreamWriter sw = new StreamWriter(
+                    @"C:\Users\Ucenik\Desktop\datoteka.txt"))
+            {
+                sw.WriteLine("Ime: { 0}" + ime);
+                sw.WriteLine("Prezime: { 0}" + prezime);
+            }
+
+            Console.WriteLine("\n-- Pročitano iz datoteke:");
+            using (StreamReader sr = new StreamReader(
+                   @"C:\Users\Ucenik\Desktop\datoteka.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+            Console.ReadKey();
+            
+
+        }
+    }
+}
+
---
+++ HeskiAntonioNaredbaUsing/HeskiAntonioNaredbaUsing/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HeskiAntonioNaredbaUsing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HeskiAntonioNaredbaUsing")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b1008b19-c890-42d8-93bf-abbb2e93ce1c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
